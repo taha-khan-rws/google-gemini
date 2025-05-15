70cfccb563d7f1b550cf9261e584be21b702d669
taha-khan-rws <taha.sait@outlook.com>
2025-05-15 08:16:53 +0530

    remove api keys from gemini.js and removing console logs

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,10 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
-// import { configDotenv } from "dotenv";
 
-// const ai = new GoogleGenAI({ apiKey: "AIzaSyAeZseeQc7ih_cZFE3q0GTFzhjyIzkdt-o" });
 const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
-console.log(import.meta.env)
-// const apiKey = configDotenv.VITE_GEMINI_API_KEY;
 console.log(apiKey)
 const ai = new GoogleGenAI({ apiKey });
 async function run(prompt) {
